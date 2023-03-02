4817c483a196872a76297d075688a90519afaf09
Lucas <115025043+Lucas72002@users.noreply.github.com>
2023-03-02 16:33:53 -0300

    Add files via upload

---
+++ User Fitness Survey.py
@@ -0,0 +1,12 @@
+frequency = "once a week"
+intensity = "low"
+
+highly_active = frequency == "daily"
+print("Highly active user:")
+print(highly_active)
+
+high_intensity = intensity == "high"
+print("High intensity sports:")
+print(high_intensity)
+
+#String comparisons to label data acquired through a survey.
